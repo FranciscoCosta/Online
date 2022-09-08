3e9102510d4f703e3bb7a0028dc8aab7f70fc50b
Francisco Costa <francisco100eg@gmail.com>
2022-09-08 15:30:15 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const resultados = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const api = await resultados.json();
+  return api;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const resultados = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const api = await resultados.json();
+  return api;
 }
 
 export async function getProductById() {
