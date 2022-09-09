1da7184f9aa13fc629b3b7721cd685ca1c87356d
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-09-09 18:27:31 -0300

    Requisito 9

--- src/components/Categorias.jsx
+++ src/components/Categorias.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getCategories } from '../services/api';
+//
 
 export default class Categorias extends Component {
   state = {
